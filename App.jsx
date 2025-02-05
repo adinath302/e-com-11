a4fa25bb6db75173eaf19e2172d079d745e09a44
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-02-05 21:31:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './component/Navbar/Navbar.jsx'
+import Hero from './component/Hero/Hero.jsx'
+import Category from "./component/Category/Category.jsx"
+import Category2 from "./component/Category/Category2.jsx"
+import Services from './component/Services/Services.jsx'
+import Banner from './component/Banner/Banner.jsx'
+import headphone from "./assets/hero/headphone.png"
+import Products from "./component/Products/Products.jsx"
+
+const BannerData = {
+  discount: "30% oFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "lorem ipsumm, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#f42c37"
+}
+
+const App = () => {
+  return (
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
+      <Navbar />
+      <Hero />
+      <Category />
+      <Category2 />
+      <Services />
+      <Banner data={BannerData} />
+      <Products />
+    </div>
+  )
+}
+
+export default App
---
+++ component/Products/ProductCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ProductCard = ({ data }) => {
+    return (
+        <div className='mb-10'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 place-items-center'>
+                {/* Card section    */}
+                {
+                    data.map((data) => (
+                        <div key={data.id}>
+                            <div>
+                                <img src={data.img} alt="" className='[h-180px] w-[260px] object-cover rounded-md' />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
